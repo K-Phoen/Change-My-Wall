bf47ff3eded8abcf31f1c5e30d361f439c1cace0
Kévin <contact@kevingomez.fr>
2014-03-07 22:54:59 +0100

    Declare onclick events in the view.xml

--- src/activity/MainActivity.java
+++ src/activity/MainActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ImageButton;
 
 import com.cmw.R;
 
@@ -13,22 +11,17 @@ public class MainActivity extends Activity {
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);    
+       super.onCreate(savedInstanceState);
+
        setContentView(R.layout.main_view);
-       initButtonsListeners();
     }
 	
-	protected void initButtonsListeners () {
-		ImageButton settingsBtn = (ImageButton) findViewById(R.id.settingsBtn);
-		final Activity self = this;
-		
-		settingsBtn.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent (self, ProvidersActivity.class);
-				startActivity(intent);
-			}
-		});
-		
+	public void editSettings(View view) {
+		Intent intent = new Intent (this, ProvidersActivity.class);
+		startActivity(intent);
+	}
+	
+	public void changeWallpaper(View view) {
+		System.out.println("coucou");
 	}
 }
