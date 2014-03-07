36ab9540d417afdc4388e957a366215be29b9ce2
Kotylive13 <Hokage-Echizen@hotmail.fr>
2014-03-07 22:24:07 +0100

    Added Action Buttons and new providers activity

--- src/activity/MainActivity.java
+++ src/activity/MainActivity.java
@@ -1,7 +1,11 @@
 package activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 import com.cmw.R;
 
@@ -11,5 +15,20 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);    
        setContentView(R.layout.main_view);
-    }	
+       initButtonsListeners();
+    }
+	
+	protected void initButtonsListeners () {
+		ImageButton settingsBtn = (ImageButton) findViewById(R.id.settingsBtn);
+		final Activity self = this;
+		
+		settingsBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent (self, ProvidersActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+	}
 }
---
+++ src/activity/ProvidersActivity.java
@@ -0,0 +1,15 @@
+package activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cmw.R;
+
+public class ProvidersActivity extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);    
+       setContentView(R.layout.provider_view);
+    }	
+}
