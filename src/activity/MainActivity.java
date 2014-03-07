58c85864acc1d6b32d2d671c61f905328f4353e8
Kotylive13 <Hokage-Echizen@hotmail.fr>
2014-03-07 21:50:06 +0100

    Add placeholders for wallpaper title and author

--- src/activity/MainActivity.java
+++ src/activity/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends Activity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);    
-
        setContentView(R.layout.main_view);
     }	
 }
