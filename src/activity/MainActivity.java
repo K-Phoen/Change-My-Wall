f7462e743a23b35dd641a0b9919f39547a8b1b54
Kévin <contact@kevingomez.fr>
2014-03-09 10:58:01 +0100

    Fix wallpaper view update on change event

--- src/activity/MainActivity.java
+++ src/activity/MainActivity.java
@@ -1,13 +1,15 @@
 package activity;
 
-
 import settings.SettingsRepository;
 import wallpaper.entity.Wallpaper;
 import wallpaper.repository.ResultCallback;
 import wallpaper.repository.WallpaperRepository;
 import android.app.Activity;
 import android.app.WallpaperManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 
@@ -22,7 +24,7 @@ public class MainActivity extends Activity {
 	SettingsRepository settings;
 
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		settings = new SettingsRepository(new DatabaseHandler(this));
@@ -31,26 +33,36 @@ public class MainActivity extends Activity {
 
 		setContentView(R.layout.main_view);
 
-		GestureImageView wallpaperView = (GestureImageView) findViewById(R.id.wallpaperImage);
+		final GestureImageView wallpaperView = (GestureImageView) findViewById(R.id.wallpaperImage);
 		wallpaperView.setImageDrawable(wallpaperManager.peekDrawable());
-    }
+
+		IntentFilter filter = new IntentFilter();
+		filter.addAction(Intent.ACTION_WALLPAPER_CHANGED);
+
+		BroadcastReceiver receiver = new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				System.out.println("lolilol");
+				wallpaperView.setImageDrawable(wallpaperManager.getDrawable());
+			}
+		};
+		registerReceiver(receiver, filter);
+	}
 
 	public void editSettings(View view) {
 		Intent intent = new Intent(this, ProvidersActivity.class);
 		startActivity(intent);
 	}
-	
-	public void changeWallpaper(View view) {
-		final GestureImageView wallpaperView = (GestureImageView) findViewById(R.id.wallpaperImage);
 
+	public void changeWallpaper(View view) {
 		if (settings.getCurrentProviderName() != null) {
-			wallpaperRepository.selectProvider(settings.getCurrentProviderName());
+			wallpaperRepository.selectProvider(settings
+					.getCurrentProviderName());
 		}
 
 		wallpaperRepository.changeWallpaper(this, new ResultCallback() {
 			@Override
 			public void handleResult(Wallpaper wallpaper) {
-				wallpaperView.setImageDrawable(wallpaperManager.peekDrawable());				
 			}
 		});
 	}
